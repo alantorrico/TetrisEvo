fe7dc49602d729f569034105db95a96f669abdd2
alantorrico <alantorrico@gmail.com>
2013-09-14 11:36:22 -0400

    Creado Paquete Modelo
    Contien Bloque Pieza

---
+++ TetrisEvo/src/com/jawaresoft/tetrisevo/modelos/Bloque.java
@@ -0,0 +1,15 @@
+package com.jawaresoft.tetrisevo.modelos;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Bloque {
+	
+	public TextureRegion texturaRegion;
+
+	protected Pieza piezas[][] = new Pieza[3][3];
+	
+	public Bloque(){
+		
+	}
+
+}
