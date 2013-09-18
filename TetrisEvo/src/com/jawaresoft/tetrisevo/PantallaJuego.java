f872c6c5ca9f670e909612e0c6ad52b0215ecc01
WilmerTerrazas <wilmer_tv_96@hotmail.es>
2013-09-18 14:46:11 -0400

    Agregado Movimiento del Personaje
    Se puede mover el personaje a la izquierda y derecha

--- TetrisEvo/src/com/jawaresoft/tetrisevo/PantallaJuego.java
+++ TetrisEvo/src/com/jawaresoft/tetrisevo/PantallaJuego.java
@@ -1,5 +1,11 @@
 package com.jawaresoft.tetrisevo;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL11;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class PantallaJuego extends Pantalla {
@@ -13,6 +19,7 @@ public class PantallaJuego extends Pantalla {
 	}
 
 	public void render(float delta) {
+		Gdx.gl.glClear(GL11.GL_COLOR_BUFFER_BIT);
 		escenario.act();
 		escenario.draw();
 	}
@@ -21,10 +28,54 @@ public class PantallaJuego extends Pantalla {
 		escenario = new Stage();
 		personaje = new Personaje();
 		bala = new Bala();
+		Gdx.input.setInputProcessor(escenario);
 		
 		escenario.addActor(personaje);
 		escenario.addActor(bala);
+		
 		personaje.setPosition(50, 50);
+		escenario.setKeyboardFocus(personaje);
+		personaje.addListener(new InputListener(){
+
+			@Override
+			public boolean keyDown(InputEvent event, int keycode) {
+				switch(keycode){
+				case Input.Keys.RIGHT:
+					personaje.velocidad.x = 150;
+					return true;
+				case Input.Keys.LEFT:
+					personaje.velocidad.x = -150;
+					return true;
+				default:
+					return false;
+				}
+			}
+
+			@Override
+			public boolean keyUp(InputEvent event, int keycode) {
+				boolean respuesta = true;
+				switch(keycode){
+				case Input.Keys.RIGHT:
+					personaje.velocidad.x = 0;
+				case Input.Keys.LEFT:
+					personaje.velocidad.x = 0;
+				default:
+					respuesta = false;
+				}
+				return respuesta;
+			}
+
+			@Override
+			public boolean keyTyped(InputEvent event, char character) {
+				// TODO Auto-generated method stub
+				return super.keyTyped(event, character);
+			}
+			
+		});
+		
+		
+		
+		
 		bala.setPosition(80, 110);
 	}
 
@@ -34,5 +85,11 @@ public class PantallaJuego extends Pantalla {
 	public void dispose() {
 		escenario.dispose();
 	}
+
+	@Override
+	public void resize(int width, int height) {
+		escenario.setViewport(width, height, true);
+	}
+	
 
 }
--- TetrisEvo/src/com/jawaresoft/tetrisevo/Personaje.java
+++ TetrisEvo/src/com/jawaresoft/tetrisevo/Personaje.java
@@ -3,11 +3,38 @@ package com.jawaresoft.tetrisevo;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Personaje extends Actor {
 	
 	private TextureRegion imagen;
+	public Vector2 velocidad = new Vector2(0, 0);
+
+	@Override
+	public void act(float delta) {
+		translate(velocidad.x*delta, velocidad.y*delta);
+		
+		if(getX() < 0){
+			setX(0);
+			velocidad.x = 0;
+		}
+		else if(getRight() > getStage().getWidth()){
+			setX(getStage().getWidth() - getWidth());
+			velocidad.x = 0;
+		}
+		
+		if(getY() < 0){
+			setY(0);
+			velocidad.y = 0;
+		}
+		else if(getTop() > getStage().getHeight()){
+			setY(getStage().getHeight() - getHeight());
+			velocidad.y = 0;
+		}
+		
+		
+	}
 
 	public Personaje() {
 		imagen = new TextureRegion(TetrisEvo.ADMINISTRADOR_RECURSOS.get("Personaje.png", Texture.class), 64, 64);
